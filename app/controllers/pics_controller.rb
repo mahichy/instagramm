a3269726b5679346c545b470fddf4f5a17b464a4
mahi <mohiddinoshim@yahoo.com>
2021-01-15 20:02:02 +0600

    Show and Edit

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,10 @@
 class PicsController < ApplicationController
-
+	before_action :find_pic, only: [:show, :edit, :update, :destroy]
 	def index
+		
+	end
+
+	def show
 		
 	end
 
@@ -10,6 +14,12 @@ class PicsController < ApplicationController
 
 	def create
 		@pic = Pic.new(pic_params)
+
+		if @pic.save
+			redirect_to @pic, notice: "Yes, it was posted!"
+		else
+			render "new"
+		end
 	end
 
 
@@ -17,6 +27,10 @@ class PicsController < ApplicationController
 
 	def pic_params
 		params.require(:pic).permit(:title, :description)
+	end
+
+	def find_pic
+		@pic = Pic.find(params[:id])
 	end
 	
 end
